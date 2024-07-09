06bd6bc64e829c60411a47e27c5d1f0f77d64f9d
AbhinandNtk <abhinandntkm@gmail.com>
2024-07-09 13:00:34 +0530

    updated basic app file by adding the get and post reqests

--- Flask_Framework/app.py
+++ Flask_Framework/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 '''
 It creates a instance of the flask class,
 Which will be your WSGI(Webserver Gateway interface) application
@@ -14,9 +14,19 @@ def welocme():
 def Html():
     return "<html><H1>Testing<H1><html>"
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+## Flask get and post operations
+@app.route('/test',methods=['GET','POST'])
+def Postinfo():
+    if request.method =='POST':
+       print("Method:-",request.method)
+       print('first_name:-',request.form['fname'])
+   
+    return render_template('form.html')
+
 
 
 
