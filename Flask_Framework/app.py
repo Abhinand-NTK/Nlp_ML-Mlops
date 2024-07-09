05e191f6e3d2ca2c8e236642a31971d5a1124c53
AbhinandNtk <abhinandntkm@gmail.com>
2024-07-09 12:15:13 +0530

    updated basic app file configration of flask

--- Flask_Framework/app.py
+++ Flask_Framework/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 '''
 It creates a instance of the flask class,
 Which will be your WSGI(Webserver Gateway interface) application
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route("/")
 def welocme():
     return "welocme to flask app.This should be a great"
+
+@app.route("/with_html")
+def Html():
+    return "<html><H1>Testing<H1><html>"
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 
